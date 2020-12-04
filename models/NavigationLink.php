70b6136c8f571ac89c242cd7e8a22462473b47ae
Sarah-Maziers <sarah.maziers@student.hepl.be>
2020-12-04 13:05:45 +0100

    added NavigationLink.php model

---
+++ models/NavigationLink.php
@@ -0,0 +1,17 @@
+<?php
+
+Class NavigationLink extends Model{
+    public $url;
+    public $label;
+    public $icon;
+    //ce que le model doit faire
+public function __construct($url,$label,$icon)
+{
+    $this->icon;
+     $this->label;
+     $this->url;
+
+}
+
+}
+
